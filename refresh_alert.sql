e522ea3ae9ebe179e3b3963aea06371e0ddacead
Cong Liu <cl3720@aqua.dbmi.columbia.edu>
2022-03-16 17:48:52 -0400

    put restrain on alert log

--- refresh_alert.sql
+++ refresh_alert.sql
@@ -16,4 +16,8 @@ DELETE FROM emerge4cuimclocalredcap.redcap_alerts_sent_log
 WHERE alert_sent_id in 
 (
 SELECT alert_sent_id FROM emerge4cuimclocalredcap.redcap_alerts_sent
+where alert_id in
+(SELECT alert_id FROM emerge4cuimclocalredcap.redcap_alerts
+where project_id = 33
+)
 );
