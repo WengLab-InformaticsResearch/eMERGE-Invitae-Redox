f534e3ad4027b2214ca18c7ce77af0ec59648e70
Cong Liu <cl3720@aqua.dbmi.columbia.edu>
2022-03-16 17:45:48 -0400

    add alert refresh for test

---
+++ refresh_alert.sql
@@ -0,0 +1,19 @@
+-- The purpose is to refresh the alert setting to see whether the alert works as expected.
+-- Should not use this in a production server b/c all the alert will be triggered again
+DELETE FROM emerge4cuimclocalredcap.redcap_alerts_sent
+where alert_id in
+(SELECT alert_id FROM emerge4cuimclocalredcap.redcap_alerts
+where project_id = 33
+);
+
+DELETE FROM emerge4cuimclocalredcap.redcap_alerts_recurrence
+where alert_id in
+(SELECT alert_id FROM emerge4cuimclocalredcap.redcap_alerts
+where project_id = 33
+);
+
+DELETE FROM emerge4cuimclocalredcap.redcap_alerts_sent_log
+WHERE alert_sent_id in 
+(
+SELECT alert_sent_id FROM emerge4cuimclocalredcap.redcap_alerts_sent
+);
