d8c1de54cc90df95049ec5f72b2f363e06e84370
Cong Liu <cl3720@aqua.dbmi.columbia.edu>
2022-03-23 16:38:02 -0400

    project setup fix sequencial form issue

--- project_setup.py
+++ project_setup.py
@@ -1,9 +1,5 @@
 import requests
 import json
-from pprint import pprint
-from datetime import datetime
-import pandas as pd
-from io import StringIO
 
 
 api_token_file = './api_tokens.json'
@@ -40,16 +36,44 @@ print('HTTP Status: ' + str(r.status_code))
 # remove field already existing.
 meta_local_json_field_name_list = [i['field_name'] for i in meta_local_json]
 meta_r4_json_deduplicated = [i for i in meta_r4_json if i['field_name'] not in meta_local_json_field_name_list]
+
+# new_json = []
+# form_name_current = ""
+# for meta_local_field in meta_local_json:
+#     form_name_new = meta_local_field['form_name']
+#     if (form_name_new == form_name_current) or form_name_current == "":
+#         new_json.append(meta_local_field)
+#         form_name_current = form_name_new
+#     else:
+#         for meta_r4_field in meta_r4_json_deduplicated:
+#             if meta_r4_field['form_name'] == form_name_current:
+#                 new_json.append(meta_r4_field)
+#         form_name_current = form_name_new
+
 meta_json = meta_local_json + meta_r4_json_deduplicated
 # redcap requires form in sequential order.
-meta_json.sort(key=lambda x: x["form_name"])
+# meta_json.sort(key=lambda x: x["form_name"]) # this will change the order and re-assign the record id.
+ordered_form_names = []
+for i in meta_json:
+    if i['form_name'] not in ordered_form_names:
+        ordered_form_names.append(i['form_name'])
+new_json = []
+for i in ordered_form_names:
+    for j in meta_json:
+        if j['form_name'] == i:
+            new_json.append(j)
+
+with open('./test_meta.json','w') as f:
+    json.dump(new_json,f)
+
 # update local data dictionary
+# IMPORTANT: need to re-arrange the 
 data = {
     'token': api_key_local,
     'content': 'metadata',
     'format': 'json',
     'returnFormat': 'json',
-    'data': json.dumps(meta_json)
+    'data': json.dumps(new_json)
 }
 r = requests.post(cu_local_endpoint,data=data)
 print('HTTP Status: ' + str(r.status_code))
